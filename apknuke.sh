455abca4e2e4911c0b7ef2bcd08c9a1a7a89c0ef
Utkarsh Sharma <54320208+utkarsh24122@users.noreply.github.com>
2021-08-18 14:29:44 +0530

    Update apknuke.sh

--- apknuke.sh
+++ apknuke.sh
@@ -16,5 +16,9 @@ pwd | (read foo; echo "$foo"/"$target"_decompile | nuclei --silent -t $PATH_TO_N
 
 if [[ $(cat ./"$target"_nuclei_output/nuk.txt | wc -l) -le 1 ]]; then
     echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m No results ... Better Luck Next time \e[0m\n" 
-  else echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Results Saved in "$target"_nuclei_output/nuk.txt \e[0m\n" 
+  else echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Results Saved in "$target"_nuclei_output/ \e[0m\n" 
+cat "$target"_nuclei_output/nuk.txt | grep "info" | sort -u >> "$target"_nuclei_output/info.txt
+cat "$target"_nuclei_output/nuk.txt | grep -v "info" | sort -u >> "$target"_nuclei_output/non-info.txt
+cat "$target"_nuclei_output/nuk.txt | grep "firebase-database" | sort -u >> "$target"_nuclei_output/non-info.txt
+rm "$target"_nuclei_output/nuk.txt
 fi
